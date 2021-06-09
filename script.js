637d866ec94c9eeae099bb7dec1e5a330306d692
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2021-06-09 14:35:30 +0200

    Alt fonts in place, commented out

--- script.js
+++ script.js
@@ -50,13 +50,13 @@ let iconBank = new Array(
 // );
 
 let textBank = [
-  `Be long, not too long, short, not too short. Too difficult? Come - lets sleep. We can do the rest in the morning. She says no, there is something left to do. The gap between us, tired, yawns.`,
+  `Be long, not too long, short, not too short. Too difficult. Come - lets sleep. We can do the rest in the morning. She says no. There is much, so much, left to do. I go to bed, carefree.`,
   `The pillow is a magnetic negative pole. My head is a positive one. I took a Xanax and now I'm stuck. The room lights up, a shock to my narcotic stillness. A message on my phone. Worse yet, an emoji.`,
-  `Sometimes the moonlight is like your mothers milk, primary and caressing. There is no world. All pain ends.`,
+  `Sometimes the moonlight is like your mothers milk, primary and caressing. All pain eventually ends.`,
   `Joshua, Marike and Mish were sitting in the park. It was the dead of night. Mish had swallowed the world. Marike was trying the Heimlich. Josh expelled gas. Mish laughed, Atlas came flying out. It worked!`,
-  `A single column in the night. Not too bright a glow. With my eyes closed I didn't notice. The ghost of an end - far overreached but never attained - smiles at me. I snore, sleeping.`,
-  `Celestial objects hang on threads. Will I ever know them? The threat of morning has no bite at this hour. I look up, to where I've looked so many times, the same threads pulling me. `,
-  `They hover around my pillow, to remind me when I return. Some are like puffs of perfume. Some are like the updraft from the sewer. They say "The Night's sleep is a life that lives forever!" I wonder if they will ever make an app that will turn them into films.`,
+  `A single column in the night. Not too bright a glow. With my eyes closed I didn't notice. The ghost of an ambition - far overreached but never attained - smiles at me. I snore, sleeping.`,
+  `Celestial objects hang on threads. Will I ever know them? The threat of morning has no bite at this hour. I look up, to where I've looked so many times. The same threads pull me. `,
+  `They hover around my pillow, to remind me when I return. Some are like puffs of perfume. Some are like the updraft from a sewer. They say, "The Night's sleep is a life that lives forever!" They know I will turn them into books.`,
 ];
 
 function randomPicIconText() {
